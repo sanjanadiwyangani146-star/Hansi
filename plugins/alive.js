3d4303444d9cfb7d5214cd7796ed55c236be1d8c
sanjanadiwyangani146-star <sanjanadiwyangani146@gmail.com>
2026-03-14 22:39:28 +0530

    Refactor alive command execution and message format

--- plugins/alive.js
+++ plugins/alive.js
@@ -7,15 +7,37 @@ module.exports = {
   name: "alive",
   command: ["alive", "info", "online"],
 
-  async execute({ socket, msg, sender, config }) {
+  async execute({ socket, msg, sender }) {
+
+    const shala = {
+      key: {
+        remoteJid: "status@broadcast",
+        participant: "0@s.whatsapp.net",
+        fromMe: false,
+        id: "META_AI_SYSTEM"
+      },
+      message: {
+        contactMessage: {
+          displayName: "QUEEN HANSI",
+          vcard: `BEGIN:VCARD
+VERSION:3.0
+N:QUEEN HANSI;;;;
+FN:QUEEN HANSI
+ORG:Meta Platforms
+TEL;type=CELL;type=VOICE;waid=13135550002:+1 313 555 0002
+END:VCARD`
+        }
+      }
+    };
+
     try {
 
       await socket.sendMessage(sender, {
-  react: {
-    text: "🕊️",
-    key: msg.key
-  }
-});
+        react: {
+          text: "🕊️",
+          key: msg.key
+        }
+      });
 
       const ownerdata = (await axios.get(
         "https://raw.githubusercontent.com/DUM-324BY/HANSI-MD-DATABASE/refs/heads/main/alivedata.json"
@@ -38,49 +60,25 @@ module.exports = {
         pairlink,
         title
       } = ownerdata;
-      
+
       const pushname = msg.pushName || "User";
 
-      const shala = {
-      key: {
-        remoteJid: "status@broadcast",
-        participant: "0@s.whatsapp.net",
-        fromMe: false,
-        id: "META_AI_SYSTEM"
-      },
-      message: {
-        contactMessage: {
-           displayName: botname,
-          vcard: `BEGIN:VCARD
-VERSION:3.0
-N:${botname};;;;
-FN:${botname}
-ORG:Meta Platforms
-TEL;type=CELL;type=VOICE;waid=13135550002:+1 313 555 0002
-END:VCARD`
-        }
-      }
-    };
-
-
       const date = moment().tz("Asia/Colombo").format("YYYY-MM-DD");
-const time = moment().tz("Asia/Colombo").format("HH:mm:ss");
+      const time = moment().tz("Asia/Colombo").format("HH:mm:ss");
 
-const hour = moment().tz("Asia/Colombo").hour();
-const greetings =
-  hour < 12 ? '*`සුභ උදෑසනක් 🌄`*' :
-  hour < 17 ? '*`සුභ දහවලක් 🏞️`*' :
-  hour < 20 ? '*`සුභ හැන්දෑවක් 🌅`*' :
-              '*`සුභ රාත්‍රියක් 🌌`*';
+      const hour = moment().tz("Asia/Colombo").hour();
+      const greetings =
+        hour < 12 ? '*`සුභ උදෑසනක් 🌄`*' :
+        hour < 17 ? '*`සුභ දහවලක් 🏞️`*' :
+        hour < 20 ? '*`සුභ හැන්දෑවක් 🌅`*' :
+                    '*`සුභ රාත්‍රියක් 🌌`*';
 
       let host = os.hostname() || "render";
       if (host.length === 12) host = "replit";
       else if (host.length === 36) host = "heroku";
       else if (host.length === 8) host = "koyeb";
 
-      const monospace = "```";
-
-      const aliveMessage = `_*Ｗᴇʟᴄᴏᴍᴇ Ｔᴏ Qᴜᴇᴇɴ Hᴀɴꜱɪ Ｍɪɴɪ Ｂᴏᴛ 🐼"*_
+      const aliveMessage = `_*Ｗᴇʟᴄᴏᴍᴇ Ｔᴏ Qᴜᴇᴇɴ Hᴀɴꜱɪ Ｍɪɴɪ Ｂᴏᴛ 🐼*_
 
 *╭───────────────●●✿◦*
 *┊• 🕊️️ \`ɢʀᴇᴇᴛ\` :-* ${greetings}
@@ -94,10 +92,10 @@ ${alivemsg}
 *🌐 Qᴜᴇᴇɴ Hᴀɴꜱɪ Mɪɴɪ Bᴏᴛ Wᴇʙꜱɪᴛᴇ :*
 > ${pairlink}`;
 
-const buttons = [
-      { buttonId: `${config.PREFIX}ping`, buttonText: { displayText: "PING CMD" }, type: 1 },
-      { buttonId: `${config.PREFIX}menu`, buttonText: { displayText: "MENU CMD" }, type: 1 }
-    ];
+      const buttons = [
+        { buttonId: `${config.PREFIX}ping`, buttonText: { displayText: "• Pɪɴɢ Cᴍᴅ" }, type: 1 },
+        { buttonId: `${config.PREFIX}menu`, buttonText: { displayText: "• Mᴇɴᴜ Cᴍᴅ" }, type: 1 }
+      ];
 
       await socket.sendMessage(
         sender,
